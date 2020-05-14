02b9c0519bb56ae53d1e3b8a090d49759abc69bf
Mark Moore <mark.moore@revature.com>
2020-05-14 15:46:52 -0500

    adds the Pipeline Model

---
+++ pipelineMvcDemo/Models/Pipeline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pipelineMvcDemo.Models
+{
+    public class Pipeline
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Throughput { get; set; }
+
+
+    }
+}
