244d09c1b33f7650336bb36267b77e36ea9d801e
Mark Moore <mark.moore@revature.com>
2020-05-14 15:52:04 -0500

    adds another property  to Pipeline Model

--- pipelineMvcDemo/Models/Pipeline.cs
+++ pipelineMvcDemo/Models/Pipeline.cs
@@ -10,6 +10,8 @@ namespace pipelineMvcDemo.Models
         public int Length { get; set; }
         public int Width { get; set; }
         public int Throughput { get; set; }
+        public int Depth { get; set; }
+
 
 
     }
