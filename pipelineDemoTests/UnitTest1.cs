4a19e4f089a774bf2b98c2811053043a751846f3
Mark Moore <mark.moore@revature.com>
2020-05-20 23:51:43 -0500

    added coverlet and GUID's for code coverage.

---
+++ pipelineDemoTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace pipelineDemoTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ pipelineDemoTests1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace pipelineDemoTests1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
